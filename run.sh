1af3df8cde40ef02210833ac276c09cdb0a219ad
Autumn <ffzhihua@126.com>
2019-01-31 11:12:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,18 @@ tar zxvf node_exporter-0.17.0.linux-amd64.tar.gz
 
 mv node_exporter-0.17.0.linux-amd64 node_exporter
 
-nohup /opt/node_exporter/node_exporter & 
+echo "[Unit]
+Description=auto run prometheus node_exporter
+[Service]
+Type=simple
+ExecStart=/opt/node_exporter/node_exporter
+Restart=always 
+KillMode=process 
+RestartSec=1
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/node_exporter.service
+
+systemctl start node_exporter 
+
+systemctl status node_exporter 
