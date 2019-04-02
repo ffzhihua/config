6cba61813bb5bc01a879d1e4343b74019e81a565
ffzhihua <ffzhihua@126.com>
2019-04-02 11:05:22 +0800

    add prometheus

---
+++ prometheus/prometheus.sh
@@ -0,0 +1,6 @@
+wget https://github.com/prometheus/prometheus/releases/download/v2.8.1/prometheus-2.8.1.linux-amd64.tar.gz
+tar zxvf prometheus-2.6.1.linux-amd64.tar.gz -C /opt
+cd /opt
+mv prometheus-2.6.1.linux-amd64 prometheus
+cd prometheus
+
